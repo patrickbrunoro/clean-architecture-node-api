3c1c0970a7f874ef021f1a2c5cfe8c884448bf38
Patrick <patrick@usemobile.com.br>
2020-06-17 03:51:29 -0300

    test: ensure SignupCOntroller return an error if no name is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -13,5 +13,6 @@ describe('SignUp Ccntroller', () => {
     }
     const httpResponse = sut.handler(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
