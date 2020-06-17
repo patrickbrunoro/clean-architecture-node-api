4466c88409a92598153e83c6466f17b9560e5de3
Patrick <patrick@usemobile.com.br>
2020-06-17 04:21:24 -0300

    refactor: move duplicate code to helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,13 +1,14 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helpers'
 
 export class SignUpController {
   handler (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return { statusCode: 400, body: new MissingParamError('name') }
+      return badRequest(new MissingParamError('name'))
     }
     if (!httpRequest.body.email) {
-      return { statusCode: 400, body: new MissingParamError('Missing param: email') }
+      return badRequest(new MissingParamError('email'))
     }
   }
 }
