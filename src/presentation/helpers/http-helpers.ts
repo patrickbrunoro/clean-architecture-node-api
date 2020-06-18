c7170e0411c1b819643ecc502f165d0541b4da41
Patrick <patrick@usemobile.com.br>
2020-06-18 02:01:13 -0300

    refactor: move sucess http to a helper method

--- src/presentation/helpers/http-helpers.ts
+++ src/presentation/helpers/http-helpers.ts
@@ -1,5 +1,5 @@
 import { HttpResponse } from '../protocols/http'
-import { ServerError } from '../errors/server-error'
+import { ServerError } from '../errors'
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
@@ -8,4 +8,8 @@ export const badRequest = (error: Error): HttpResponse => ({
 export const serverError = (): HttpResponse => ({
   statusCode: 500,
   body: new ServerError()
+})
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
 })
