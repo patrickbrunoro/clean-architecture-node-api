b178566fde01ead42b4961f0d08d9ab2ec94659a
Patrick <patrick@usemobile.com.br>
2020-06-19 00:41:39 -0300

    test: ensure BcryptAdapter call bcrypt wiht correct values

--- src/infra/criptography/bcrypt.adapter.spec.ts
+++ src/infra/criptography/bcrypt.adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt.adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await Promise.resolve('hash')
+  }
+}))
+
 describe('Bcrypt Adapter', () => {
   const salt = 12
   const makeSut = (): BcryptAdapter => {
@@ -12,5 +18,11 @@ describe('Bcrypt Adapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('Should return a hash on success', async () => {
+    const sut = makeSut()
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hash')
   })
 })
