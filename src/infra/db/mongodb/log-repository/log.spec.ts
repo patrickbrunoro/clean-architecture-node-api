b6abea66c79d788a19d81d42b3ed2546fc8abc2e
Patrick <patrick@usemobile.com.br>
2020-06-20 02:41:22 -0300

    refactor: move sut creation to a helper factory

--- src/infra/db/mongodb/log-repository/log.spec.ts
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -2,6 +2,9 @@ import { MongoHelper } from '../helpers/mongo-helper'
 import { Collection } from 'mongodb'
 import { LogMongoRepository } from './log'
 
+const makeSut = (): LogMongoRepository => {
+  return new LogMongoRepository()
+}
 describe('Log Mongo Repository', () => {
   let errorCollection: Collection
   beforeAll(async () => {
@@ -17,9 +20,6 @@ describe('Log Mongo Repository', () => {
     await errorCollection.deleteMany({})
   })
 
-  const makeSut = (): LogMongoRepository => {
-    return new LogMongoRepository()
-  }
   test('Should create an error log on success', async () => {
     const sut = makeSut()
     await sut.logError('any_error')
