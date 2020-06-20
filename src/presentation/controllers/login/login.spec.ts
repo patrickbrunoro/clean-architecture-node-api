cc1fa9eccd39676bc7c354004fcacafb7018eef1
Patrick <patrick@usemobile.com.br>
2020-06-20 13:26:21 -0300

    refactor: move sut creation to helper factory

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -2,12 +2,20 @@ import { LoginController } from './login'
 import { badRequest } from '../../helpers/http-helpers'
 import { MissingParamError } from '../../errors'
 
-describe('Login Controller', () => {
-  const makeSut = (): LoginController => {
-    return new LoginController()
+interface SutTypes{
+  sut: LoginController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new LoginController()
+  return {
+    sut
   }
+}
+
+describe('Login Controller', () => {
   test('Should return 400 if no e-mail is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         password: 'password'
@@ -17,7 +25,7 @@ describe('Login Controller', () => {
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
   test('Should return 400 if no password is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com'
