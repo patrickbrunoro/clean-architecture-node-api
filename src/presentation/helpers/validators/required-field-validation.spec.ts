41f9ed611997c21754b839b4808a1c6aefe66486
Patrick <patrick@usemobile.com.br>
2020-06-20 17:42:07 -0300

    test: ensure RequiredFieldValidation return a MissingParamError if validation feils

---
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -0,0 +1,13 @@
+import { RequiredFieldValidation } from './required-field-validation'
+import { MissingParamError } from '../../errors'
+
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation('field')
+}
+describe('RequiredField Validation', () => {
+  test('Should return a MissingParamError if validation feils', () => {
+    const sut = makeSut()
+    const error = sut.validate({ name: 'any_field' })
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+})
