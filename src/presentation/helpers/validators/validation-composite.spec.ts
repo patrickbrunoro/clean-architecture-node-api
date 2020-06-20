84809a8f70d1a5863f2d5625f4a98ecc3ab9c342
Patrick <patrick@usemobile.com.br>
2020-06-20 17:58:55 -0300

    test: ensure ValidationComposite not returns if validation success

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -38,4 +38,10 @@ describe('Validation Composite', () => {
     const error = sut.validate({ field: 'any_field' })
     expect(error).toEqual(new Error())
   })
+
+  test('Should not return if validation success', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_field' })
+    expect(error).toBeFalsy()
+  })
 })
