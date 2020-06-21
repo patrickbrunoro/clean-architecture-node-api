3a1594321fbd252ac83e1b68e6176f83b5d9f6d9
Patrick <patrick@usemobile.com.br>
2020-06-21 20:35:28 -0300

    test: ensure SurveyMongoRepository returns an account on add success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,38 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { SurveyMongoRepository } from './survey-mongo-repository'
+import { AddSurveyModel } from '../../../../domain/usecases/add-survey'
+
+let surveyCollection: Collection
+const makeFakeSurveyData = (): AddSurveyModel => ({
+  question: 'any_question',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer'
+  }]
+})
+describe('Survey Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  const makeSut = (): SurveyMongoRepository => {
+    return new SurveyMongoRepository()
+  }
+
+  test('Should add a survey on success', async () => {
+    const sut = makeSut()
+    await sut.add(makeFakeSurveyData())
+    const survey = await surveyCollection.findOne({ question: 'any_question' })
+    expect(survey).toBeTruthy()
+  })
+})
