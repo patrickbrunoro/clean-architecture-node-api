9d751f5fc28dec2d9b800396a25b1ac4f52cb941
Patrick <patrick@usemobile.com.br>
2020-06-21 04:03:07 -0300

    chore: merging

--- src/main/routes/signup-routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import { makeSignupController } from '../factories/signup'
-import { adapterRoute } from '../adapters/express-route-adapter'
-
-export default (router: Router): void => {
-  router.post('/signup', adapterRoute(makeSignupController()))
-}
