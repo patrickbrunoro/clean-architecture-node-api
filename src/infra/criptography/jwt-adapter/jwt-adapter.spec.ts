a1ac1c7954dba158d110bfd5eb85cb0cf185297c
Patrick <patrick@usemobile.com.br>
2020-06-22 02:13:10 -0300

    test: ensure JwtAdapter call verify with correct values

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -4,8 +4,12 @@ import { JwtAdapter } from './jwt-adapter'
 jest.mock('jsonwebtoken', () => ({
   async sign (): Promise<string> {
     return await Promise.resolve('any_token')
+  },
+  async verify (token: string): Promise<string> {
+    return await Promise.resolve('any_value')
   }
 }))
+
 const makeSut = (): JwtAdapter => {
   return new JwtAdapter('secret')
 }
@@ -30,6 +34,14 @@ describe('Jwt Adapter', () => {
       jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
       const promise = sut.encrypt('any_id')
       await expect(promise).rejects.toThrow()
+    })
+  })
+  describe('verify()', () => {
+    test('Should call verify with correct values', async () => {
+      const sut = makeSut()
+      const verifySpy = jest.spyOn(jwt, 'verify')
+      await sut.decrypt('any_token')
+      expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
     })
   })
 })
