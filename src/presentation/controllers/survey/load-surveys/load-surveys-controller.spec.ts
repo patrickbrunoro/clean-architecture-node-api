7218ec3b00ef893546d01b2233811c65ab1eb904
Patrick <patrick@usemobile.com.br>
2020-06-24 03:30:04 -0300

    test: ensure LoadSurveysController returns 500 LoadSurveys throws

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -2,7 +2,7 @@ import { SurveyModel } from './load-survey-controller-protocols'
 import { LoadSurveysController } from './load-surveys-controller'
 import { LoadSurveys } from '../../../../domain/usecases/load-surveys'
 import MockDate from 'mockdate'
-import { ok } from '../../../helpers/http/http-helpers'
+import { ok, serverError } from '../../../helpers/http/http-helpers'
 
 const makeFakeSurveys = (): SurveyModel[] => {
   return [
@@ -63,9 +63,17 @@ describe('LoadSurveys Controller', () => {
     await sut.handle({})
     expect(loadSpy).toHaveBeenCalledWith()
   })
+
   test('Should return 200 on success', async () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(ok(makeFakeSurveys()))
+  })
+
+  test('Should return 500 LoadSurveys throws', async () => {
+    const { sut, loadSurveysStub } = makeSut()
+    jest.spyOn(loadSurveysStub, 'load').mockReturnValueOnce(Promise.reject(new Error()))
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
