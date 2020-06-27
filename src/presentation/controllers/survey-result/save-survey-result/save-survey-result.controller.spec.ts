5bf42367eea696a3b967cb8834263e36b8547f21
Patrick <patrick@usemobile.com.br>
2020-06-27 14:06:22 -0300

    feat: ensure SaveSurveyResultController returns 500 LoadSurveys throws

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result.controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result.controller.spec.ts
@@ -2,7 +2,7 @@ import { SaveSurveyResultController } from './save-survey-result.controller'
 import { HttpRequest } from '@/presentation/protocols'
 import { LoadSurveyById } from '@/domain/usecases/survey/load-surveys-by-id'
 import { SurveyModel } from '@/domain/models/survey'
-import { forbidden } from '@/presentation/helpers/http/http-helpers'
+import { forbidden, serverError } from '@/presentation/helpers/http/http-helpers'
 import { InvalidParamError } from '@/presentation/errors'
 
 const makeFakeRequest = (): HttpRequest => ({
@@ -55,5 +55,12 @@ describe('SaveSurveyResultController', () => {
     jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(Promise.resolve(null))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')))
+  })
+
+  test('Should return 500 LoadSurveys throws', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(Promise.reject(new Error()))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result.controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result.controller.ts
@@ -1,15 +1,19 @@
 import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './save-survey-result.controller-protocols'
-import { forbidden } from '@/presentation/helpers/http/http-helpers'
+import { forbidden, serverError } from '@/presentation/helpers/http/http-helpers'
 import { InvalidParamError } from '@/presentation/errors'
 
 export class SaveSurveyResultController implements Controller {
   constructor (private readonly loadSurveyById: LoadSurveyById) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
-    if (!survey) {
-      return forbidden(new InvalidParamError('surveyId'))
+    try {
+      const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+      if (!survey) {
+        return forbidden(new InvalidParamError('surveyId'))
+      }
+      return null
+    } catch (e) {
+      return serverError(e)
     }
-    return null
   }
 }
