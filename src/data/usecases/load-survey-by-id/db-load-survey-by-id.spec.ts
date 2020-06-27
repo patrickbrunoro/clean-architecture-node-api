f74b69178334b0cda00889e1ce3ac0e27cdf4184
Patrick <patrick@usemobile.com.br>
2020-06-27 11:01:52 -0300

    refactor: add data usecases protocol files to import

--- src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -1,7 +1,6 @@
-import { SurveyModel } from '@/domain/models/survey'
-import MockDate from 'mockdate'
-import { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id-repository'
+import { LoadSurveyByIdRepository, SurveyModel } from './db-load-survey-by-id-protocols'
 import { DbLoadSurveyById } from '@/data/usecases/load-survey-by-id/db-load-survey-by-id'
+import MockDate from 'mockdate'
 
 const makeFakeSurvey = (): SurveyModel => {
   return {
