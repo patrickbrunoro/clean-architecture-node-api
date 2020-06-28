a51ad8cac2bdda193b6a54825b7ab3dd8b7e5eea
Patrick <patrick@usemobile.com.br>
2020-06-28 20:07:15 -0300

    refactor: move factory to a helper folder

--- src/data/test/mock-db-survey-result.ts
+++ src/data/test/mock-db-survey-result.ts
@@ -1,37 +1,16 @@
-import { AddSurveyRepository } from '@/data/protocols/db/survey/add-survey-repository'
-import { AddSurveyParams } from '@/domain/usecases/survey/add-survey'
-import { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id-repository'
-import { SurveyModel } from '@/domain/models/survey'
-import { mockSurveyModel, mockSurveyModels, mockSurveyResultModel } from '@/domain/test'
-import { LoadSurveysRepository } from '@/data/protocols/db/survey/load-surveys-repository'
+import { SaveSurveyResultRepository } from '@/data/protocols/db/survey-result/save-survey-result-repository'
 import { SurveyResultModel } from '@/domain/models/survey-result'
+import { mockSurveyResultModel } from '@/domain/test'
+import { SaveSurveyResultParams } from '@/domain/usecases/survey-result/save-survey-result'
 import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
 
-export const mockAddSurveyRepository = (): AddSurveyRepository => {
-  class AddSurveyRepositoryStub implements AddSurveyRepository {
-    async add (surveyData: AddSurveyParams): Promise<void> {
-      return await Promise.resolve(null)
+export const mockSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
+  class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
+    async save (data: SaveSurveyResultParams): Promise<SurveyResultModel> {
+      return await Promise.resolve(mockSurveyResultModel())
     }
   }
-  return new AddSurveyRepositoryStub()
-}
-
-export const mockLoadSurveyByIdRepository = (): LoadSurveyByIdRepository => {
-  class LoadSurveyByIdRepositoryStub implements LoadSurveyByIdRepository {
-    async loadById (): Promise<SurveyModel> {
-      return await Promise.resolve(mockSurveyModel())
-    }
-  }
-  return new LoadSurveyByIdRepositoryStub()
-}
-
-export const mockLoadSurveysRepository = (): LoadSurveysRepository => {
-  class LoadSurveysRepositoryStub implements LoadSurveysRepository {
-    async loadAll (): Promise<SurveyModel[]> {
-      return await Promise.resolve(mockSurveyModels())
-    }
-  }
-  return new LoadSurveysRepositoryStub()
+  return new SaveSurveyResultRepositoryStub()
 }
 
 export const mockLoadSurveyResultRepository = (): LoadSurveyResultRepository => {
