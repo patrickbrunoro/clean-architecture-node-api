09761ee736871251a873ad73440ac92bedd74e6f
Patrick <patrick@usemobile.com.br>
2020-06-27 23:42:19 -0300

    test: ensure save survey route result returns 403 on save survey result without accessToken

---
+++ src/main/routes/survey-result-routes.test.ts
@@ -0,0 +1,24 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+
+describe('SurveyResult Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  describe('PUT /surveys/:surveyId/results', () => {
+    test('Should return 403 on save survey result without accessToken', async () => {
+      await request(app)
+        .put('/api/surveys/any_id/results')
+        .send({
+          answers: 'any_answer'
+        })
+        .expect(403)
+    })
+  })
+})
