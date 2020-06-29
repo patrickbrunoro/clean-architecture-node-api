4e524909e99aced8b9a63a4fd9d9b48cf99b1e3f
Patrick <patrick@usemobile.com.br>
2020-06-29 00:24:03 -0300

    docs: update docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,6 +1,13 @@
-import paths from './paths'
-import schemas from './schemas'
-import components from './components'
+import { loginPath, signUpPath, surveyPath, surveyResultPath } from '@/main/docs/paths/'
+import {
+  accountSchema, addSurveyParamsSchema, addSurveyResultParamsSchema, apiKeyAuthSchema,
+  errorSchema,
+  loginParamsSchema, signUpParamsSchema,
+  surveyAnswerSchema, surveyResultSchema,
+  surveySchema,
+  surveysSchema
+} from '@/main/docs/schemas/'
+import { badRequest, forbidden, notFound, serverError, unauthorized } from '@/main/docs/components/'
 
 export default {
   openapi: '3.0.0',
@@ -17,8 +24,33 @@ export default {
   }, {
     name: 'Enquete'
   }],
-  paths: paths,
-  schemas: schemas,
-  components: components
+  paths: {
+    '/login': loginPath,
+    '/signup': signUpPath,
+    '/surveys': surveyPath,
+    '/surveys/{surveyId}/results': surveyResultPath
+  },
+  schemas: {
+    account: accountSchema,
+    surveys: surveysSchema,
+    survey: surveySchema,
+    surveyAnswer: surveyAnswerSchema,
+    loginParams: loginParamsSchema,
+    signUpParams: signUpParamsSchema,
+    addSurveyParams: addSurveyParamsSchema,
+    addSurveyResultParams: addSurveyResultParamsSchema,
+    surveyResult: surveyResultSchema,
+    error: errorSchema
+  },
+  components: {
+    securitySchemes: {
+      apiKeyAuth: apiKeyAuthSchema
+    },
+    badRequest,
+    unauthorized,
+    forbidden,
+    notFound,
+    serverError
+  }
 
 }
